b48469b695d3cf89391f04450eb6f3cc50156050
Kevin Unkrich <kevinunkrich@Kevins-MacBook-Pro.local>
2022-07-10 19:46:00 -0700

    Update before post

--- .zshrc
+++ .zshrc
@@ -1,23 +1,12 @@
+## ALIASES ##
 alias g="git"
 
-# Path to your oh-my-zsh installation.
+## ZSH SETUP ##
 export ZSH=$HOME/.oh-my-zsh
-
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="robbyrussell"
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
-
 
 # Homebrew
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
--- setup.sh
+++ setup.sh
@@ -15,15 +15,12 @@ EOF
   exit 0
 fi
 
-CURRDIR=`pwd`
-BREWINSTALLED=`which brew`
-XCODEINSTALLED=`which xcode-select`
-
 
 ###################################
 # XCODE                           #
 ###################################
 
+XCODEINSTALLED=`which xcode-select`
 if [[ ${XCODEINSTALLED} == "" ]]; then
   echo "Installing Xcode"
   xcode-select --install
@@ -37,21 +34,33 @@ fi
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     rm ~/.zshrc
-    ln -s $CURRDIR/zshrc-config ~/.zshrc
+    ln -s $PWD/.zshrc ~/.zshrc
 fi
-zsh
-# TODO: ZSH theme install
+chsh -s /bin/zsh
 
 
 ###################################
-# HOMEBREW / APPS                 #
+# HOMEBREW / LANGUAGES            #
 ###################################
 
-if [[ ${BREWINSTALLED} == "" ]]; then
-  echo "Installing homebrew and applications"
-  sh .homebrew
-  sh .dev
-fi
+sh .homebrew
+sh .dev
+
+
+###################################
+# NVIM                            #
+###################################
+
+mkdir -p ~/.config/nvim/lua
+ln -s $PWD/init.lua ~/.config/nvim/init.lua
+echo "lua require('init')" >> ~/.config/nvim/init.vim
+
+
+###################################
+# TMUX                            #
+###################################
+
+ln -s $PWD/.tmux.conf ~/.tmux.conf
 
 
 ###################################
@@ -60,37 +69,30 @@ fi
 
 sh .macos
 
-# TODO: sh git.sh
+
+###################################
+# YABAI / SKHD / Spacebar         #
+###################################
+
+brew install koekeishiya/formulae/yabai
+ln -s $PWD/.yabairc ~/.yabairc
+brew services start yabai
+
+brew install koekeishiya/formulae/skhd
+ln -s $PWD/.skhdrc ~/.skhdrc
+brew services start skhd
+
+brew install cmacrae/formulae/spacebar
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+brew services start spacebar
 
 # TODO: Install Chrome Extensions
 # Paywall Blocker, 1Pass, React DevTools
-
-# TODO: Copy and setup dotfiles
-# tmux, nvim, zshrc
-
-# TODO: Yabai, skhd
 
 # TODO: /box setup
 # https://wiki.nikiv.dev/backups/
 
 # TODO: Automatically start tmux on opening terminal (automatically save tmux on exit of terminal?)
 
-
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
-
-# TODO: Update list
-for app in "Activity Monitor" \
-	"Dock" \
-	"Finder" \
-	"Google Chrome" \
-	"Messages" \
-	"Photos" \
-	"Twitter" \
-	"iCal"; do
-	killall "${app}" &> /dev/null
-done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
-
 touch ~/.osx-bootstrapped.txt
